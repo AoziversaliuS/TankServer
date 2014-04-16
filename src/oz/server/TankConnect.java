e2cd9164291c3dbd693de266294ec82ce5d49aea
AoziversaliuS <1220310838@qq.com>
2014-04-17 07:29:24 +0800

    重新写

---
+++ src/oz/server/TankConnect.java
@@ -0,0 +1,23 @@
+package oz.server;
+
+import java.net.Socket;
+
+import oz.bean.Tank;
+
+public class TankConnect {
+	
+	Socket socket;
+	
+	
+	public Tank call(){
+		
+		return null;
+	}
+	
+	
+	public void send(){
+		
+	}
+	
+	
+}
